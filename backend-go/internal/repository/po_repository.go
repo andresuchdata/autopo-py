9cf1a9d57a7ff7709ab65b498065b5c2010e58dc
Andre Suchitra <andresuch.data@gmail.com>
2025-11-30 21:51:55 +0700

    master data endpoints brands and stores

--- backend-go/internal/repository/po_repository.go
+++ backend-go/internal/repository/po_repository.go
@@ -11,4 +11,5 @@ type PORepository interface {
 	SavePOResults(ctx context.Context, storeName string, results []*domain.POResult) error
 	GetStoreResults(ctx context.Context, storeName string) ([]*domain.POResult, error)
 	GetStores(ctx context.Context) ([]*domain.Store, error)
+	GetBrands(ctx context.Context) ([]*domain.Brand, error)
 }
